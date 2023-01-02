6ef789ff3665ea452c124eff0020a06243a932fe
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-01-02 16:16:09 +0100

    format.sh use `git status` instead of `git diff`
    - Help with README table resize

--- format.sh
+++ format.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Format newly added files using clang-format
-# Note: must run before git add
 
-for i in $(git diff README.md | grep '+' | cut -d'|' -f2 -s)
+for i in $(git status | grep Problems | xargs)
 do
   echo "Formating: $i"
-  clang-format -i ./Problems/$i.cpp
+  clang-format -i $i
 done
