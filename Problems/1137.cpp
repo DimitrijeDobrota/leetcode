17aabb25ba1a5be13448189c828e6741748b99c3
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-01-21 15:22:34 +0100

    Dynamic Programming I: Day 1

--- Problems/1137.cpp
+++ Problems/1137.cpp
@@ -1,3 +1,4 @@
+// memorization approach
 class Solution {
 public:
   int tribonacci(int n) {
@@ -7,5 +8,22 @@ public:
     f[2] = 1;
     for (int i = 3; i <= n; i++) f[i] = f[i - 1] + f[i - 2] + f[i - 3];
     return f[n];
+  }
+};
+
+// optimized, memorize only the previous three values
+class Solution {
+public:
+  int tribonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int a = 0, b = 1, c = 1;
+    for (int i = 3; i <= n; i++) {
+      int tmp = a + b + c;
+      a = b;
+      b = c;
+      c = tmp;
+    }
+    return c;
   }
 };
