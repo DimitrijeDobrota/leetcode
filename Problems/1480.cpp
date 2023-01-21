5fd55bcf1031021e7ce5dba8f0e8fb833a9d7262
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-01-21 15:54:42 +0100

    LeetCode 75 I: Day 1

--- Problems/1480.cpp
+++ Problems/1480.cpp
@@ -7,3 +7,13 @@ public:
     return res;
   }
 };
+
+// using lambda function
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int acc = 0;
+        for_each(nums.begin(), nums.end(), [&acc](int &a) { a = acc+=a; });
+        return nums;
+    }
+};
