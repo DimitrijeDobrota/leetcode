4342f7511e21103b79c9e605241ae41f134cee85
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-01-22 21:29:08 +0100

    Data Structure I: Day 1

--- Problems/0053.cpp
+++ Problems/0053.cpp
@@ -1,3 +1,4 @@
+// memorization approach
 class Solution {
 public:
   int maxSubArray(vector<int> &nums) {
@@ -9,6 +10,17 @@ public:
       dp[i] = nums[i] + max(dp[i - 1], 0);
       maxi = max(maxi, dp[i]);
     }
+    return maxi;
+  }
+};
+
+// optimized, memorize only the previous value
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int n = nums.size(), maxi, prev;
+    maxi = prev = nums[0];
+    for (int i = 1; i < n; i++) maxi = max(maxi, prev = nums[i] + max(prev, 0));
     return maxi;
   }
 };
--- Problems/0217.cpp
+++ Problems/0217.cpp
@@ -10,3 +10,12 @@ public:
     return false;
   }
 };
+
+// Fun oneliner
+class Solution {
+public:
+  bool containsDuplicate(vector<int> &nums) {
+    return nums.size() > unordered_set<int>(nums.begin(), nums.end()).size();
+  }
+};
+
