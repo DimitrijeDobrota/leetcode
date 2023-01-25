37001bb0f0d8f22b999cef91bac4cd8d8b34e035
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-01-25 01:16:06 +0100

    Ignore deleted files

--- format.sh
+++ format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Format newly added files using clang-format
 
-for i in $(git status | grep -Eo "(Problems|Templates).*\.cpp")
+for i in $(git status | grep -v "deleted" | grep -Eo "(Problems|Templates).*\.cpp")
 do
   echo "Formating: $i"
   clang-format -i $i
