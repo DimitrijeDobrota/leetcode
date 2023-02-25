d73581d247cf1608adebfb2b20eb570f5ab66892
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-02-25 15:21:31 +0100

    Random Problem

---
+++ Problems/0009.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+    int sum = 0;
+    do { sum = sum * 10 + x % 10; } while ((x /= 10) > sum);
+    return (x == sum) || (x == sum / 10);
+  }
+};
