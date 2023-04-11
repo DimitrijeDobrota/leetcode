8561c37ee0c619ca1b21fbce159c647ae2739b2d
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-04-11 09:52:19 +0200

    Daily Problem

--- Problems/2390.cpp
+++ Problems/2390.cpp
@@ -1,3 +1,4 @@
+// Stack solution
 class Solution {
 public:
   string removeStars(string s) {
@@ -15,5 +16,40 @@ public:
     }
     reverse(res.begin(), res.end());
     return res;
+  }
+};
+
+// Deque solution, avoid reversal
+class Solution {
+public:
+  string removeStars(string s) {
+    deque<char> dq;
+    for (const char &c : s)
+      if (c == '*')
+        dq.pop_back();
+      else
+        dq.push_back(c);
+
+    string res = "";
+    while (!dq.empty()) {
+      res += dq.front();
+      dq.pop_front();
+    }
+    return res;
+  }
+};
+
+// Two pointer, constant space, solution
+class Solution {
+public:
+  string removeStars(string s) {
+    int i = 0;
+    for (int j = 0; j < s.size(); j++) {
+      if (s[j] == '*')
+        i--;
+      else
+        s[i++] = s[j];
+    }
+    return s.substr(0, i);
   }
 };
