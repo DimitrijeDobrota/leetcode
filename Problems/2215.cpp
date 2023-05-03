e107602f2b2ba9d52f158fb497a94fe08a073750
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-05-03 11:07:54 +0200

    Daily Problem

---
+++ Problems/2215.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  vector<vector<int>> findDifference(vector<int> &nums1, vector<int> &nums2) {
+    unordered_set<int> s1(nums1.begin(), nums1.end());
+    unordered_set<int> s2(nums2.begin(), nums2.end());
+    vector<vector<int>> res(2);
+    for (auto num : s1)
+      if (!s2.count(num)) res[0].push_back(num);
+    for (auto num : s2)
+      if (!s1.count(num)) res[1].push_back(num);
+    return res;
+  }
+};
