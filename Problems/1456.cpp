b217fbac194589a7e335e0fd4a3f5af91bee7f7d
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-05-05 11:16:44 +0200

    Daily Problem

---
+++ Problems/1456.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  bool isVowel(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+  }
+
+public:
+  int maxVowels(string s, int k) {
+    int i, cnt = 0;
+    for (i = 0; i < k; i++)
+      if (isVowel(s[i])) cnt++;
+
+    int maxi = cnt;
+    for (; i < s.size(); i++) {
+      if (isVowel(s[i - k])) cnt--;
+      if (isVowel(s[i])) cnt++;
+      maxi = max(maxi, cnt);
+    }
+
+    return maxi;
+  }
+};
