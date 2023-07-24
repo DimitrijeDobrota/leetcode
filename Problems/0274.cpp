e4c546d8852c1f9f8a06571a723ea2b3346ac4f9
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-07-24 19:48:10 +0200

    Random Problem

---
+++ Problems/0274.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  static int arr[5001];
+
+public:
+  int hIndex(vector<int> &citations) {
+    memset(arr, 0x00, sizeof(arr));
+    for (int n : citations) arr[n]++;
+
+    int total = 0;
+    for (int i = 5000; i >= 0; i--) {
+      total += arr[i];
+      if (total >= i) return i;
+    }
+
+    return -1;
+  }
+};
+
+int Solution::arr[5001] = {0};
