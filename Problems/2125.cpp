b4cb2b84000edff8d93699cde3c33998d60ad826
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2024-01-03 16:33:48 +0000

    Improved Daily Problem

--- Problems/2125.cpp
+++ Problems/2125.cpp
@@ -1,19 +1,12 @@
-// 2125. Number of Laser Beams in a Bank
 class Solution {
-  public:
-    int numberOfBeams(const vector<string> &bank) {
-        vector<int> count;
-        count.reserve(bank.size());
-        for (const auto &floor : bank) {
+public:
+    int numberOfBeams(const vector<string>& bank) const {
+        int res = 0, prev = 0;
+        for(const auto& floor: bank) {
             int cnt = 0;
-            for (char c : floor)
-                if (c == '1') cnt++;
-            if (cnt) count.push_back(cnt);
+            for(const char c: floor) cnt += c == '1';
+            if(cnt) res += cnt * prev, prev = cnt;
         }
-
-        int res = 0;
-        for (int i = 1; i < count.size(); i++)
-            res += count[i] * count[i - 1];
         return res;
     }
 };
